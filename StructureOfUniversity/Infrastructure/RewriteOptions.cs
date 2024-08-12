5a276473beb01d8ef8683999559306077ec3979b
Marina Melnikova <108426963+Melchalk@users.noreply.github.com>
2024-08-12 11:22:54 +0300

    lab 13 (#13)

---
+++ StructureOfUniversity/Infrastructure/RewriteOptions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Rewrite;
+using StructureOfUniversity.DTOs.Enums;
+
+namespace StructureOfUniversity.Infrastructure;
+
+public static class RewriterOptions
+{
+    private const string UserPosition = "UserPosition";
+
+    public static void RewriteRequests(RewriteContext context)
+    {
+        var httpContext = context.HttpContext;
+
+        var position = httpContext.Items[UserPosition];
+        var path = httpContext.Request.Path.Value;
+
+        if (position is null || path is null)
+        {
+            return;
+        }
+
+        if (position.ToString() == TeachingPositions.Assistant.ToString() &&
+            path.Contains("faculty/update"))
+        {
+            string proccedPath = path.Replace("update", "get/all");
+
+            httpContext.Response.ContentType = "application/json";
+
+            httpContext.Request.Path = new PathString(proccedPath);
+            httpContext.Request.Method = "GET";
+
+            httpContext.Request.RouteValues["action"] = "GetFaculties";
+        }
+
+    }
+}
